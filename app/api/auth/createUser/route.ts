3758e5d17dee17a01c5bf912719399bb974a7518
garunAMT <21ME02022@iitbbs.ac.in>
2024-09-29 23:00:04 +0530

    modified callback urls after deployment for user auth

--- app/api/auth/createUser/route.ts
+++ app/api/auth/createUser/route.ts
@@ -1,31 +1,34 @@
 import prisma from "@/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
-import { unstable_noStore  as noStore} from "next/cache";
+import { unstable_noStore as noStore } from "next/cache";
 
-export async function GET(){
-    noStore();
-    const {getUser} = getKindeServerSession();
-    const user = await getUser();
+export async function GET() {
+  noStore();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    if(!user || user==null || !user.id) 
-        throw new Error("User not found");
+  if (!user || user == null || !user.id) throw new Error("User not found");
 
-    let dbUser = await prisma.user.findUnique({
-        where: {
-            id: user.id
-        }
-    })
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
 
-    if(!dbUser){
-        dbUser = await prisma.user.create({
-            data: {
-                id: user.id,
-                email: user.email ?? "",
-                name: user.given_name ?? "",
-            }
-        })
-    }
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        email: user.email ?? "",
+        name: user.given_name ?? "",
+      },
+    });
+  }
 
-    return NextResponse.redirect(process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://jazzee.io");
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://jazzee-marketplace.vercel.app/"
+  );
 }
