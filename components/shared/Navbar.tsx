6c84898913ab4334a05ef3a8c7feae3f052408c3
garunAMT <21ME02022@iitbbs.ac.in>
2024-10-15 09:44:25 +0530

    fixed the mobile navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -28,72 +28,70 @@ export default async function Navbar() {
       <div className="flex w-full lg:w-0 items-center justify-between">
         <Sheet>
           <SheetTrigger asChild>
-            <Button variant="outline" size="icon" className="lg:hidden">
-              <Menu className="h-6 w-6" />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
+        <Button variant="outline" size="icon" className="lg:hidden">
+          <Menu className="h-6 w-6" />
+          <span className="sr-only">Toggle navigation menu</span>
+        </Button>
           </SheetTrigger>
           <SheetContent side="left">
-            <SheetHeader>
-              <SheetTitle>Jazzee Market</SheetTitle>
-            </SheetHeader>
-            <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
-              {/* <MountainIcon className="h-6 w-6" /> */}
-              {/* <div className="text-xl bg-red-300">Acme Inc</div> */}
-              <span className="sr-only">Jazzee Market</span>
-            </Link>
-            <div className="grid gap-2 py-6">
-              <Link
-                href="/"
-                className="flex w-full items-center py-2 text-base"
-                prefetch={false}
-              >
-                Home
-              </Link>
-              <Link
-                href="/profile"
-                className="flex w-full items-center py-2 text-base"
-                prefetch={false}
-              >
-                Profile
-              </Link>
-              <Link
-                href="/products"
-                className="flex w-full items-center py-2 text-base"
-                prefetch={false}
-              >
-                Products
-              </Link>
-              <Link
-                href={"/requests"}
-                className="flex w-full items-center py-2 text-base"
-                prefetch={false}
-              >
-                Requests
-              </Link>
-              <Link
-                href={user ? "/create-auction" : "/api/auth/login"}
-                className="flex w-full items-center py-2 text-base"
-                prefetch={false}
-              >
-                Create Auction
-              </Link>
-            </div>
+        <SheetHeader>
+          <SheetTitle>Jazzee Market</SheetTitle>
+        </SheetHeader>
+        <Link href="/" className="mr-6 hidden lg:flex" prefetch={false}>
+          <span className="sr-only">Jazzee Market</span>
+        </Link>
+        <div className="grid gap-2 py-6">
+          <Link
+            href="/"
+            className="flex w-full items-center py-2 text-base"
+            prefetch={false}
+          >
+            Home
+          </Link>
+          <Link
+            href="/products"
+            className="flex w-full items-center py-2 text-base"
+            prefetch={false}
+          >
+            Products
+          </Link>
+          <Link
+            href={user ? "/create-auction" : "/api/auth/login"}
+            className="flex w-full items-center py-2 text-base"
+            prefetch={false}
+          >
+            Create Auction
+          </Link>
+          <Link
+            href="/dashboard"
+            className="flex w-full items-center py-2 text-base"
+            prefetch={false}
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/my-auctions"
+            className="flex w-full items-center py-2 text-base"
+            prefetch={false}
+          >
+            My Auctions
+          </Link>
+        </div>
           </SheetContent>
         </Sheet>
 
         <div className="flex items-center gap-3 lg:hidden">
           {user ? (
-            <ProfileDropdown
-              picture={
-                user?.picture ||
-                "https://static.vecteezy.com/system/resources/previews/009/292/244/original/default-avatar-icon-of-social-media-user-vector.jpg"
-              }
-            />
+        <ProfileDropdown
+          picture={
+            user?.picture ||
+            "https://static.vecteezy.com/system/resources/previews/009/292/244/original/default-avatar-icon-of-social-media-user-vector.jpg"
+          }
+        />
           ) : (
-            <Button asChild>
-              <LoginLink>Sign in</LoginLink>
-            </Button>
+        <Button asChild>
+          <LoginLink>Sign in</LoginLink>
+        </Button>
           )}
           {/* <ModeToggle /> */}
         </div>
